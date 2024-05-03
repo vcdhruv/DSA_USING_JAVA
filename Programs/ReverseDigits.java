d3c358cf37ac76345b089ae1b58330f3fd699395
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-03 12:25:55 +0530

    Upadet reverse number function

--- Programs/ReverseDigits.java
+++ Programs/ReverseDigits.java
@@ -7,10 +7,12 @@ public class ReverseDigits {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int n = sc.nextInt() , temp = n , ans = 0;
+        if(n < 0) temp = temp * -1;
         while(temp > 0){
             ans = 10 * ans + temp % 10;
             temp = temp / 10;
         }
+        if(n < 0) ans = ans * -1;
         System.out.println("Reverse Number Of " + n + " is : " + ans);
     }
 }
