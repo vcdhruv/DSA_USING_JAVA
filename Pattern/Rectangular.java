2be53dd5f4336c2daaea1bd6128c13e98cd0905f
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-03 15:54:08 +0530

    Created Pattern Folder and rectangular pattern 1

---
+++ Pattern/Rectangular.java
@@ -0,0 +1,19 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class Rectangular {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows : ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns : ");
+        int columns = sc.nextInt();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
