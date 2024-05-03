6dbcf36038a67fac7f1c08ea776721b6d22a9cd4
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-03 16:12:06 +0530

    Created Reverse Triangular pattern 4

---
+++ Pattern/ReverseTriangular.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class ReverseTriangular {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows : ");
+        int rows = sc.nextInt();
+        outer:for (int i = 0; i < rows; i++) {
+            inner:for (int j = rows; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
