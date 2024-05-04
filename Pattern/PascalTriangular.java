d56033416f1b9eccd8ea25a8061013d270dbd716
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-04 20:02:40 +0530

    Updated Pascal Triangle with 1 formula and without any function

--- Pattern/PascalTriangular.java
+++ Pattern/PascalTriangular.java
@@ -3,23 +3,18 @@ package Pattern;
 import java.util.Scanner;
 
 public class PascalTriangular {
-    public static int factorial(int n){
-        int fact = 1;
-        for (int i = 1; i <= n; i++) {
-            fact = fact * i;
-        }
-        return fact;
-    }
-    public static int combination(int n , int r){
-        return factorial(n) / (factorial(r) * factorial(n-r));
-    }
+    //at starting it will be 1 only;
+    //to get newvalue use icjp1 = icj * (i - j) / j+1
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size of pascal's triangle : ");
         int size = sc.nextInt();
         for (int i = 0; i < size; i++) {
+            int iCj = 1; // on every row start nC0 is always 1
             for (int j = 0; j <= i; j++) {
-                System.out.print(combination(i, j)+"\t");
+                System.out.print(iCj+"\t");
+                int iCjp1 = (iCj * (i-j))/(j+1); // to get new value from old value
+                iCj = iCjp1;
             }
             System.out.println();
         }
