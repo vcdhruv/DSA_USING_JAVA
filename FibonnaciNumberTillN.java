6304676e297e0b4f083d2836248ea8c895ac0f74
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-04 11:53:17 +0530

    Fibonnaci Number Till N

---
+++ FibonnaciNumberTillN.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FibonnaciNumberTillN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        int a = 0 , b = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+    }
+}
