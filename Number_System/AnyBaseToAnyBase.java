bfcbb66e77708eb766c06ab9dff760986eb2bd5d
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-06 00:28:16 +0530

    Updated Any base to any base

--- Number_System/AnyBaseToAnyBase.java
+++ Number_System/AnyBaseToAnyBase.java
@@ -3,6 +3,26 @@ package Number_System;
 import java.util.Scanner;
 
 public class AnyBaseToAnyBase {
+    public static int AnyBaseToDecimal(int n, int base) {
+        int pw = 1, answer = 0;
+        while (n > 0) {
+            answer += (n % 10) * pw;
+            pw *= base;
+            n /= 10;
+        }
+        return answer;
+    }
+
+    public static int DecimalToAnyBase(int n, int base) {
+        int pw = 1, answer = 0;
+        while (n > 0) {
+            answer += (n % base) * pw;
+            pw *= 10;
+            n /= base;
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number : ");
@@ -11,19 +31,10 @@ public class AnyBaseToAnyBase {
         int base = sc.nextInt();
         System.out.print("Tell base to be converted to : ");
         int cbase = sc.nextInt();
-        int pw = 1 , answer = 0;
         int original_n = number;
-        while (number != 0) {
-            if(cbase == 10){
-                answer += (number % 10) * pw;
-                pw*=base;
-                number /= 10;
-            }else{
-                answer += (number % cbase) * pw;
-                pw*=10;
-                number /= cbase;
-            }
-        }
-        System.out.println("Answer Of "+original_n+" of base "+base+" to base "+cbase+" is : "+answer);
+        int dec = AnyBaseToDecimal(number, base);
+        System.out.println(dec);
+        int answer = DecimalToAnyBase(dec, cbase);
+        System.out.println("Answer Of " + original_n + " of base " + base + " to base " + cbase + " is : " + answer);
     }
 }
