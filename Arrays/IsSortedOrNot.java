0e5b85439367a581de3bf06a3b69792168b48688
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-06 16:25:55 +0530

    it is sorted or not

--- Arrays/IsSortedOrNot.java
+++ Arrays/IsSortedOrNot.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class IsSortedOrNot {
     public static boolean IsSortedOrNot(int[] arr){
         for (int i = 1; i < arr.length - 1; i++) {
-            if (arr[i-1] >= arr[i]) {
+            if (arr[i-1] > arr[i]) {
+                System.out.println(arr[i-1]+">"+arr[i] + " = "+(arr[i-1] > arr[i]));
                 return false;
             }
         }
