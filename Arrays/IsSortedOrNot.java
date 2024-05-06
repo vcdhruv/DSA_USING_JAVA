c82148d8baec02ed762885bec673c06c5bac59e9
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-06 16:09:42 +0530

    array is sorted or not

---
+++ Arrays/IsSortedOrNot.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class IsSortedOrNot {
+    public static boolean IsSortedOrNot(int[] arr){
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i-1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter element " + (i+1) + " : ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Is Sorted Or Not : "+IsSortedOrNot(arr));
+    }
+}
