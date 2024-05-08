dd3cd0f5984aa664a274afbdf3f007575f2befed
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-08 12:17:58 +0530

    span means difference between max and min

---
+++ Arrays/span.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class span {
+    public static int span(int[] arr){
+        int min = arr[0] , max = arr[0];
+        for (int i : arr) {
+            if (i < min) {
+                min = i;
+            }
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max-min;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter element " + (i+1) + " : ");
+            arr[i] = sc.nextInt();
+        }
+        int difference = span(arr);
+        System.out.println(difference);
+    }
+}
