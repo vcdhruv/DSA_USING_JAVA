2aa3ab9c6c4a4ba295277fef940a72985b6afaf0
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-09 12:43:48 +0530

    print all subarrays

---
+++ Arrays/SubArray.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class SubArray {
+    public static void printArray(int[] arr){
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    public static void generateAllSubArrays(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int j2 = i; j2 <= j; j2++) {
+                    System.out.print((char)arr[j2]+"\t");
+                }
+                System.out.println();
+            }
+            // System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter element " + (i+1) + " : ");
+            arr[i] = sc.nextInt();
+        }
+        generateAllSubArrays(arr);
+    }
+}
