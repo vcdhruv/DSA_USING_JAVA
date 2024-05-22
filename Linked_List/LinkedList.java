1ea55c6eaba72ce833c167cbd79e4d6be4a0490f
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-05-22 12:34:29 +0530

    added insert method

--- Linked_List/LinkedList.java
+++ Linked_List/LinkedList.java
@@ -13,6 +13,28 @@ public class LinkedList {
         Node head = null;
         Node tail = null;
 
+        void insert(int val , int idx){
+            Node temp = new Node(val);
+            Node x = head;
+            if (idx == LengthLL()) {
+                insertAtEnd(val);
+                return;
+            } else if (idx == 0) {
+                insertAtBeginning(val);
+                return;
+            } else if (idx < 0 || idx > LengthLL()) {
+                System.out.println("Wrong Input!!");
+                return;
+            }
+            int currIdx = 0;
+            while (currIdx < idx) {
+                currIdx++;
+                x = x.next;
+            }
+            temp.next = x.next;
+            x.next = temp;
+        }
+
         void insertAtEnd(int val){
             Node temp = new Node(val);
             if (head == null) {
@@ -64,6 +86,8 @@ public class LinkedList {
         ll.insertAtEnd(3);
         ll.displayLL();
         ll.insertAtBeginning(0);
+        ll.displayLL();
+        ll.insert(4, 3);
         ll.displayLL();
         System.out.println("head node : "+ll.head.data);
         System.out.println("tail node : "+ll.tail.data);
